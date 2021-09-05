88189c9db699e048151730275347fbee93c961dd
HopeHappy <hope_happy@outlook.com>
2021-09-05 15:13:17 -0400

    Implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+// Test function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Function that takes in an object and a value and returns the first key that corresponds to that value
+const findKeyByValue = function(obj, value) {
+  for (let key of Object.keys(obj)) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+};
+
+// Test code
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
