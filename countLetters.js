a4196dbffad1f33853450427d24bedd7732b2b12
HopeHappy <hope_happy@outlook.com>
2021-09-05 13:44:58 -0400

    Implement countLetters

---
+++ countLetters.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  const results = {};
+  const strWithouSpace = str.split(' ').join('');
+  
+  for (let char of strWithouSpace) {
+    if (!results[char]) {
+      results[char] = 1;
+      continue;
+    }
+    results[char]++;
+  }
+
+  return results;
+};
+
+const results = countLetters('lighthouse in the house');
+assertEqual(results.l, 1);
+assertEqual(results.i, 2);
+assertEqual(results.g, 1);
+assertEqual(results.h, 4);
+assertEqual(results.t, 2);
+assertEqual(results.o, 2);
+assertEqual(results.u, 2);
+assertEqual(results.s, 2);
+assertEqual(results.e, 3);
+assertEqual(results.n, 1);
