f924c713937cceec17204f10d69e3e1384c5847c
HopeHappy <hope_happy@outlook.com>
2021-09-14 01:05:51 -0400

    Remove commented out test code

--- test/headTest.js
+++ test/headTest.js
@@ -31,8 +31,3 @@ describe('#head', () => {
   });
 
 });
-
-// assertEqual(head([5, 6, 7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head(["Hannah"]), "Hannah");
-// assertEqual(head([]), undefined);
