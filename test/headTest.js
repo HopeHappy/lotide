7a2f9cab6ddfdc93fd88679d6038ecc6ad4734b8
HopeHappy <hope_happy@outlook.com>
2021-09-14 01:04:20 -0400

    Use Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,38 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hannah"]), "Hannah");
-assertEqual(head([]), undefined);
+describe('#head', () => {
+
+  it('returns 5 for [5, 6, 7]', () => {
+    const input = [5, 6, 7];
+    const output = head(input);
+    const expectedResult = 5;
+    assert.strictEqual(output, expectedResult);
+  });
+
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    const input = ["Hello", "Lighthouse", "Labs"];
+    const output = head(input);
+    const expectedResult = "Hello";
+    assert.strictEqual(output, expectedResult);
+  });
+
+  it('returns "Hannah" for ["Hannah"]', () => {
+    const input = ["Hannah"];
+    const output = head(input);
+    const expectedResult = "Hannah";
+    assert.strictEqual(output, expectedResult);
+  });
+
+  it('returns undefined for []', () => {
+    const input = [];
+    const output = head(input);
+    assert.isUndefined(output);
+  });
+
+});
+
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head(["Hannah"]), "Hannah");
+// assertEqual(head([]), undefined);
