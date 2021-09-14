8411268b538f8abd4d2b0904f6d55105dcd1c019
HopeHappy <hope_happy@outlook.com>
2021-09-14 00:37:03 -0400

    Refactor by removing the test code and adding module.exports

--- middle.js
+++ middle.js
@@ -16,31 +16,4 @@ const middle = function(arr) {
   return result;
 };
 
-// Test functions
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log('✅✅✅Assertion Passed: ', array1, '===', array2);
-  } else {
-    console.log('❌❌❌Assertion Failed: ', array1, '!==', array2);
-  }
-};
-
-// Test code
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
