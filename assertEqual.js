96adf657c40e46163a02868d222afbf2b0275d69
HopeHappy <hope_happy@outlook.com>
2021-09-13 23:22:44 -0400

    Refactor by removing the test code and adding require keyword

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,4 @@
+// Function that takes in 2 values and returns true if they are equal and false otherwise.
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
@@ -6,5 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,21 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+// Function that takes in an array and returns the first element in the array
 const head = function(array) {
-  for (let i = 0; i < array.length; i++) {
-    return array[0];
-  }
+  return array[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Hannah"]), "Hannah");
-assertEqual(head([]), undefined);
-
-
-
+module.exports = head;
