5a2f1484681b750b51f4843b74053b64511909f2
HopeHappy <hope_happy@outlook.com>
2021-09-13 23:33:46 -0400

    Refactor by removing the test code and adding module.exports

--- tail.js
+++ tail.js
@@ -1,25 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
+// Function that takes in an array and returns everything except the first element of the array
 const tail = function(array) {
   const newArray = array.filter(number => array.indexOf(number) !== 0);
   return newArray;
 };
 
-assertEqual(tail([1, 2, 3, 4]).length, 3);
-assertEqual(tail([1, 2, 3, 4])[0], 2);
-assertEqual(tail([1, 2, 3, 4])[1], 3);
-assertEqual(tail([1, 2, 3, 4])[2], 4);
-
-assertEqual(tail(["Hannah"]).length, 0);
-assertEqual(tail(["Hannah"])[0], undefined);
-
-
-assertEqual(tail([]).length, 0);
-assertEqual(tail([])[0], undefined);
+module.exports = tail;
