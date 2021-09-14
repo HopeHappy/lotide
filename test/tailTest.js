573c46eb9d2463f1235f9295b00afd244f9e6181
HopeHappy <hope_happy@outlook.com>
2021-09-13 23:34:16 -0400

    Add tailTest.js

---
+++ test/tailTest.js
@@ -0,0 +1,14 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+assertEqual(tail([1, 2, 3, 4]).length, 3);
+assertEqual(tail([1, 2, 3, 4])[0], 2);
+assertEqual(tail([1, 2, 3, 4])[1], 3);
+assertEqual(tail([1, 2, 3, 4])[2], 4);
+
+assertEqual(tail(["Hannah"]).length, 0);
+assertEqual(tail(["Hannah"])[0], undefined);
+
+
+assertEqual(tail([]).length, 0);
+assertEqual(tail([])[0], undefined);
