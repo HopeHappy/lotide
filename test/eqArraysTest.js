7616fdb11de0ed113518c60ae21bfb43babe2a40
HopeHappy <hope_happy@outlook.com>
2021-09-16 04:36:48 -0400

    Use Mocha and Chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,13 +1,56 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-assertEqual(eqArrays([2, [3, [4, 5, {six: 6, seven: [7, 8]},]]], [2, [3, [4, 5, {six: 6, seven: [7, 8]}]]]), true);
-assertEqual(eqArrays([2, [3, [4, 5, {six: 6, seven: [7, 8]},]]], [2, [3, [4, 5, {six: 6, seven: [7]}]]]), false);
+describe('#eqArrays', () => {
+
+  it('returns true', () => {
+    const output = eqArrays([1, 2, 3], [1, 2, 3]);
+    assert.isTrue(output);
+  });
+
+  it('returns false', () => {
+    const output = eqArrays([1, 2, 3], [3, 2, 1]);
+    assert.isFalse(output);
+  });
+
+  it('returns true', () => {
+    const output = eqArrays(["1", "2", "3"], ["1", "2", "3"]);
+    assert.isTrue(output);
+  });
+
+  it('returns false', () => {
+    const output = eqArrays(["1", "2", "3"], ["1", "2", 3]);
+    assert.isFalse(output);
+  });
+
+  it('returns true', () => {
+    const output = eqArrays([], []);
+    assert.isTrue(output);
+  });
+
+  it('returns true', () => {
+    const output = eqArrays([[2, 3], [4]], [[2, 3], [4]]);
+    assert.isTrue(output);
+  });
+
+  it('returns false', () => {
+    const output = eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]);
+    assert.isFalse(output);
+  });
+
+  it('returns false', () => {
+    const output = eqArrays([[2, 3], [4]], [[2, 3], 4]);
+    assert.isFalse(output);
+  });
+
+  it('returns true', () => {
+    const output = eqArrays([2, [3, [4, 5, { six: 6, seven: [7, 8] },]]], [2, [3, [4, 5, { six: 6, seven: [7, 8] }]]]);
+    assert.isTrue(output);
+  });
+
+  it('returns false', () => {
+    const output = eqArrays([2, [3, [4, 5, { six: 6, seven: [7, 8] },]]], [2, [3, [4, 5, { six: 6, seven: [7] }]]]);
+    assert.isFalse(output);
+  });
+
+});
